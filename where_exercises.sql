22074245634d9ae252db4db2d7eb5510a68546c7
Jesse Leffew <jesse.leffew89@gmail.com>
2021-07-27 21:44:55 -0500

    Where-Exercises

---
+++ where_exercises.sql
@@ -0,0 +1,41 @@
+USE employees;
+
+SELECT *
+FROM employees
+WHERE first_name IN ('Irena', 'Vidya', 'Maya');
+
+SELECT *
+FROM employees
+WHERE last_name LIKE 'E%';
+
+SELECT *
+FROM employees
+WHERE last_name LIKE ('%q%');
+
+SELECT *
+FROM employees
+WHERE first_name = 'Irena'
+   OR first_name = 'Vidya'
+   OR first_name = 'Maya';
+
+SELECT *
+FROM employees
+WHERE (first_name = 'Irena'
+    OR first_name = 'Vidya'
+    Or first_name = 'Maya')
+  AND gender = 'M';
+
+SELECT *
+FROM employees
+WHERE last_name LIKE 'E%'
+OR last_name Like '%E';
+
+SELECT *
+FROM employees
+WHERE last_name LIKE 'E%'
+   AND last_name Like '%E';
+
+SELECT *
+FROM employees
+WHERE last_name LIKE '%q%'
+   AND last_name NOT Like '%qu%';
