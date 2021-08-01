36c52415a6d6e7885b6070dc446678586860889b
Jesse Leffew <jesse.leffew89@gmail.com>
2021-08-01 13:52:12 -0500

    join

---
+++ join_exercises.sql
@@ -0,0 +1,9 @@
+USE employees;
+
+SELECT CONCAT(d.dept_name) AS 'Department Name'
+FROM employees.departments as e
+         JOIN dept_emp as de
+              ON de.emp_no = e.emp_no
+         JOIN departments as d
+              ON d.dept_no = de.dept_no
+WHERE de.to_date = '9999-01-01' AND e.emp_no = 10001;
