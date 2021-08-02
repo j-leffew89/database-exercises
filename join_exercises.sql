53c24d5faa09d7bf00dde58b97cb26398e119f90
Jesse Leffew <jesse.leffew89@gmail.com>
2021-08-01 21:07:17 -0500

    join exercise complete

--- join_exercises.sql
+++ join_exercises.sql
@@ -1,9 +1,45 @@
 USE employees;
 
-SELECT CONCAT(d.dept_name) AS 'Department Name'
-FROM employees.departments as e
-         JOIN dept_emp as de
-              ON de.emp_no = e.emp_no
+SELECT d.dept_name AS `Department Name`, CONCAT(e.first_name, ' ', e.last_name) AS `Department Manager`
+FROM employees as e
+         JOIN dept_manager as dm
+              ON dm.emp_no = e.emp_no
          JOIN departments as d
-              ON d.dept_no = de.dept_no
-WHERE de.to_date = '9999-01-01' AND e.emp_no = 10001;
+              ON d.dept_no = dm.dept_no
+WHERE dm.to_date = '9999-01-01';
+
+#---------------------------------------------------------------------------------------------------------------------
+
+SELECT d.dept_name AS `Department Name`, CONCAT(e.first_name, ' ', e.last_name) AS `Department Manager`
+FROM employees as e
+         JOIN dept_manager as dm
+              ON dm.emp_no = e.emp_no
+         JOIN departments as d
+              ON d.dept_no = dm.dept_no
+WHERE dm.to_date = '9999-01-01'
+  AND e.gender = 'F';
+
+#---------------------------------------------------------------------------------------------------------------------
+
+SELECT t.title AS Title, count(t.title)
+FROM titles as t
+         join dept_emp de on t.emp_no = de.emp_no
+         join departments d on d.dept_no = de.dept_no
+WHERE de.dept_no = 'd009'
+  AND t.to_date = '9999-01-01'
+GROUP BY t.title
+ORDER BY COUNT(t.title) DESC;
+
+#---------------------------------------------------------------------------------------------------------------------
+
+SELECT d.dept_name AS `Department Name`, CONCAT(e.first_name, ' ', e.last_name) AS `Department Manager`, s.salary
+FROM employees as e
+         JOIN salaries AS s
+              ON e.emp_no = s.emp_no
+                  AND s.to_date = '9999-01-01'
+         JOIN dept_manager as dm
+              ON dm.emp_no = e.emp_no
+         JOIN departments as d
+              ON d.dept_no = dm.dept_no
+WHERE dm.to_date = '9999-01-01'
+ORDER BY salary ASC;
