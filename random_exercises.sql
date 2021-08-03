7ca07796d279847d52f1afb1aa624000f1d3c07c
Jesse Leffew <jesse.leffew89@gmail.com>
2021-08-03 11:22:35 -0500

    quotable

--- random_exercises.sql
+++ random_exercises.sql
@@ -1,26 +1,3 @@
-# USE employees;
-#
-# # what are the top three most common job titles?
-# SELECT COUNT(title), title
-# FROM titles
-# GROUP BY title
-# LIMIT 3;
-#
-# # what is the lowest employee number for the first senior engineers in the company?
-# SELECT emp_no, title
-# FROM employees.titles
-# WHERE title LIKE 'Senior Engineer'
-# ORDER BY emp_no ASC
-# LIMIT 1;
-#
-# # what is the most common ‘from date’ for job titles? for staff only?
-# SELECT COUNT(from_date), title
-# FROM titles
-# WHERE title LIKE 'Staff'
-# GROUP BY title
-# ORDER BY title ASC;
-
-USE codeup_test_db;
 
 
 
