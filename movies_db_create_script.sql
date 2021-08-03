1ca75ca5a0b5a6373dc948b9c2249f88cb8afbc5
Jesse Leffew <jesse.leffew89@gmail.com>
2021-08-03 15:12:49 -0500

    movie_db

--- movies_db_create_script.sql
+++ movies_db_create_script.sql
@@ -9,16 +9,12 @@ CREATE TABLE movies
     id INT UNSIGNED NOT NULL,
     actors VARCHAR(1000) NOT NULL,
     director VARCHAR(50) NOT NULL,
-    moviePoster TEXT NOT NULL,
-    movieTitle VARCHAR(50) NOT NULL,
+    poster TEXT NOT NULL,
+    title VARCHAR(50) NOT NULL,
     genre VARCHAR(50) NOT NULL ,
-    yearMade VARCHAR(10) NOT NULL,
+    year INT UNSIGNED NOT NULL,
     plot TEXT NOT NULL,
-    rating VARCHAR(5) NOT NULL,
+    rating INT NOT NULL,
     PRIMARY KEY (id)
 );
-
-DESCRIBE movies;
-
-
 
