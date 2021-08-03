2931e28559387fd2a8b117199a5168499df4d152
Jesse Leffew <jesse.leffew89@gmail.com>
2021-08-03 12:01:13 -0500

    more

--- quotables_create_script.sql
+++ quotables_create_script.sql
@@ -2,10 +2,14 @@ CREATE DATABASE IF NOT EXISTS quotables_db;
 
 USE quotables_db;
 
-#AUTHORS
+#
+DROP TABLE IF EXISTS quote_topic;
 DROP TABLE IF EXISTS quotes;
 DROP TABLE IF EXISTS authors;
+DROP TABLE IF EXISTS topics;
 
+
+#AUTHORS
 CREATE TABLE authors
 (
     id         INT UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -14,14 +18,30 @@ CREATE TABLE authors
     PRIMARY KEY (id)
 );
 
-INSERT INTO authors(first_name, last_name) VALUES
-('Douglas', 'Adams'),
-('Mark', 'Twain'),
-('Kurt', 'Vonnegut');
+INSERT INTO authors(first_name, last_name)
+VALUES ('Douglas', 'Adams'),
+       ('Mark', 'Twain'),
+       ('Kurt', 'Vonnegut');
 
 SELECT *
 FROM authors;
 
+#TOPICS
+CREATE TABLE topics
+(
+    id   INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255),
+    PRIMARY KEY (id)
+);
+
+INSERT INTO topics(name)
+VALUES ('Space and Time'),
+       ('Humor'),
+       ('Office Life'),
+       ('Hitchiker''s Guide to the Galaxy');
+
+SELECT *
+FROM topics;
 
 #QUOTES
 CREATE TABLE quotes
@@ -30,7 +50,41 @@ CREATE TABLE quotes
     author_id INT UNSIGNED NOT NULL,
     content   TEXT         NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (author_id) REFERENCES authors(id)
+    FOREIGN KEY (author_id) REFERENCES authors (id)
 );
 
-SELECT * FROM quotes;
+INSERT INTO quotes (author_id, content)
+VALUES ((select id from authors where first_name = 'Douglas' and last_name = 'Adams'),
+        'I love deadlines. I love the whooshing noise they make as they go by.');
+INSERT INTO quotes (author_id, content)
+VALUES ((select id from authors where first_name = 'Douglas' and last_name = 'Adams'),
+        'Time is an illusion. Lunchtime doubly so.');
+INSERT INTO quotes (author_id, content)
+values ((select id from authors where first_name = 'Mark' and last_name = 'Twain'),
+        'Clothes make the man. Naked people have little or no influence on society.');
+INSERT INTO quotes (author_id, content)
+values ((select id from authors where first_name = 'Kurt' and last_name = 'Vonnegut'),
+        'The universe is a big place, perhaps the biggest.');
+INSERT INTO quotes (author_id, content)
+VALUES ((select id from authors where first_name = 'Douglas' and last_name = 'Adams'), 'Don''t Panic.');
+
+
+SELECT *
+FROM quotes;
+
+
+#QUOTE_TOPIC
+
+CREATE TABLE quote_topic
+(
+    quote_id INTEGER UNSIGNED NOT NULL,
+    topic_id INTEGER UNSIGNED NOT NULL,
+    FOREIGN KEY (quote_id) REFERENCES quotes (id),
+    FOREIGN KEY (topic_id) REFERENCES topics (id)
+);
+
+INSERT INTO quote_topic(quote_id, topic_id)
+VALUES (1, 2), (1, 3), (2, 4), (3, 1),
+       (3, 2), (3, 3), (3, 4), (4, 2), (5, 1);
+
+SELECT * FROM quote_topic;
